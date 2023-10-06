6b20f12cdcb8598eab0986b4855d6cccdc9b0848
S <s>
2023-10-06 10:28:46 +0400

    {"types":"C","runs":2}

--- problem11.java
+++ problem11.java
@@ -4,6 +4,35 @@ public class problem11 extends SuperKarel{
 
 	public void run(){
 	
+		move();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 //		move();
@@ -65,81 +94,81 @@ public class problem11 extends SuperKarel{
 		
 		
 		
-	
-	move();
-		move();
-		
-		
-		while(beepersPresent()){
-			pickBeeper();
-			turnAround();
-			move();
-			turnAround();
-			while(beepersPresent()){
-				pickBeeper();
-				move();
-				move();
-				putBeeper();
-				move();
-				putBeeper();
-				turnAround();
-				move();
-				move();
-				move();
-				turnAround();
-				
-			}
-		
-			move();
-			move();
-			move();
-			
-			move();
-			putBeeper();
-			turnAround();
-			move();
-			turnAround();
-				
-				while(beepersPresent()){
-					pickBeeper();
-					turnAround();
-					move();
-					move();
-					move();
-					putBeeper();
-					turnAround();
-					move();
-					move();
-					move();
-					
-					
-					
-				}
-			turnAround();
-			move();
-			move();
-			turnAround();
-		
-		
-		
-		}
-		
-		
-		move();
-		move();
-		move();
-		while(beepersPresent()){
-			pickBeeper();
-			turnAround();
-			move();
-			move();
-			move();
-			turnAround();
-			putBeeper();
-			move();
-			move();
-			move();	
-		}
+//	
+//	move();
+//		move();
+//		
+//		
+//		while(beepersPresent()){
+//			pickBeeper();
+//			turnAround();
+//			move();
+//			turnAround();
+//			while(beepersPresent()){
+//				pickBeeper();
+//				move();
+//				move();
+//				putBeeper();
+//				move();
+//				putBeeper();
+//				turnAround();
+//				move();
+//				move();
+//				move();
+//				turnAround();
+//				
+//			}
+//		
+//			move();
+//			move();
+//			move();
+//			
+//			move();
+//			putBeeper();
+//			turnAround();
+//			move();
+//			turnAround();
+//				
+//				while(beepersPresent()){
+//					pickBeeper();
+//					turnAround();
+//					move();
+//					move();
+//					move();
+//					putBeeper();
+//					turnAround();
+//					move();
+//					move();
+//					move();
+//					
+//					
+//					
+//				}
+//			turnAround();
+//			move();
+//			move();
+//			turnAround();
+//		
+//		
+//		
+//		}
+//		
+//		
+//		move();
+//		move();
+//		move();
+//		while(beepersPresent()){
+//			pickBeeper();
+//			turnAround();
+//			move();
+//			move();
+//			move();
+//			turnAround();
+//			putBeeper();
+//			move();
+//			move();
+//			move();	
+//		}
 		
 		
 		
