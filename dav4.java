b007712150136a4ef60601a78dac600546bcd489
S <s>
2023-10-09 16:42:28 +0400

    {"types":"C","runs":1}

--- dav4.java
+++ dav4.java
@@ -1,4 +1,29 @@
+import stanford.karel.SuperKarel;
 
-public class dav4 {
-
+public class dav4 extends SuperKarel{
+	public void run(){
+	
+	while(leftIsClear()){
+		move();
+		turnLeft();
+		move();
+		move();
+		turnRight();
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	}
 }
