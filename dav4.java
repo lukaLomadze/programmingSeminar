b605a6a9ccf230bd9e966e40e18f42bb8fdeae5d
S <s>
2023-10-09 17:14:47 +0400

    {"types":"C","runs":5}

--- dav4.java
+++ dav4.java
@@ -3,23 +3,28 @@ import stanford.karel.SuperKarel;
 public class dav4 extends SuperKarel{
 	public void run(){
 	
-	while(leftIsClear()){
-		move();
-		turnLeft();
-		move();
-		if(frontIsClear()){
-		move();
-		}
-		turnRight();
 		
 		
 		
-	}
-		turnRight();
-		while(frontIsClear()){
-			move();
-		}
-		putBeeper();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 		
 		
 		
