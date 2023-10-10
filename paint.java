811c059e65702cff4027eff0d7a803fcc1d5b49e
S <s>
2023-10-10 17:20:52 +0400

    {"types":"C","runs":15}

--- paint.java
+++ paint.java
@@ -1,10 +1,16 @@
+import java.awt.Color;
+
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class paint extends GraphicsProgram{
 	public void run(){
-GRect rect = new GRect(300, 300);
-	add(rect, 30, 100);
+	GRect rect = new GRect(200, 200);
+	rect.setFilled(true);
+	rect.setColor(Color.red);
+
+
+	add(rect, 100, 100);
 	
 	
 	}
