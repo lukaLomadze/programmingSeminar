57a0292af110ce2c2723eac3c5e045ab5c5bde7f
S <s>
2023-10-10 11:58:01 +0400

    {"types":"C","runs":13}

--- dav4.java
+++ dav4.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import acm.graphics.GRect;
 
 import acm.program.GraphicsProgram;
@@ -7,6 +9,8 @@ public class dav4 extends GraphicsProgram{
 	public void run(){
 	
 		GRect rect = new GRect (50 , 50, 100, 100);
+		rect.setFilled(true);
+		rect.setColor(Color.black);
 		add(rect);
 		
 		
