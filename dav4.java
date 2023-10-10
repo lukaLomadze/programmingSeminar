481dc11957539d06ca1416f941bd3b2a816ac472
S <s>
2023-10-10 11:52:45 +0400

    {"types":"C","runs":8}

--- dav4.java
+++ dav4.java
@@ -6,7 +6,7 @@ import stanford.karel.SuperKarel;
 public class dav4 extends GraphicsProgram{
 	public void run(){
 	
-		GRect rect = new GRect (50 , 50);
+		GRect rect = new GRect (50 , 50, 100, 100);
 		add(rect);
 		
 		
