c63d254d61bf3909c574f7c57e1e22facb72391c
S <s>
2023-10-10 11:24:00 +0400

    {"types":"C","runs":1}

--- dav4.java
+++ dav4.java
@@ -1,9 +1,10 @@
+import acm.program.ConsoleProgram;
 import stanford.karel.SuperKarel;
 
-public class dav4 extends SuperKarel{
+public class dav4 extends ConsoleProgram{
 	public void run(){
 	
-		
+		print("luka");
 		
 		
 		
