6af9d1560347b3a64565456d0696cd5d171ca53a
S <s>
2023-10-10 16:29:40 +0400

    {"types":"C","runs":1}

--- problem11.java
+++ problem11.java
@@ -5,51 +5,51 @@ public class problem11 extends SuperKarel{
 	public void run(){
 
 		
-//		move();
-//		while(beepersPresent()){
-//			move();
-//			while(beepersPresent()){	
-//			pickBeeper();
-//			
-//			turnAround();
-//			move();
-//			pickBeeper();
-//			turnAround();
-//			move();
-//			move();
-//			putBeeper();
-//			
-//			
-//			turnAround();
-//			move();
-//			
-//			turnAround();
-//			
-//			}
-//			move();
-//			move();
-//			putBeeper();
-//			turnAround();
-//			move();
-//			turnAround();
-//			while(beepersPresent()){
-//				pickBeeper();
-//				turnAround();
-//				move();
-//				putBeeper();
-//				turnAround();
-//				move();
-//				
-//			
-//			}
-//			turnAround ();
-//			move();
-//			move();
-//			turnAround();
-//			
-//			
-//			
-//		}		
+		move();
+		while(beepersPresent()){
+			move();
+			while(beepersPresent()){	
+			pickBeeper();
+			
+			turnAround();
+			move();
+			pickBeeper();
+			turnAround();
+			move();
+			move();
+			putBeeper();
+			
+			
+			turnAround();
+			move();
+			
+			turnAround();
+			
+			}
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			turnAround();
+			while(beepersPresent()){
+				pickBeeper();
+				turnAround();
+				move();
+				putBeeper();
+				turnAround();
+				move();
+				
+			
+			}
+			turnAround ();
+			move();
+			move();
+			turnAround();
+			
+			
+			
+		}		
 //		
 //		
 		
