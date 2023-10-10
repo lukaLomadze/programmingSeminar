a6257ab11281955bbac6248cc2c61bac55eeab2c
S <s>
2023-10-10 17:17:47 +0400

    {"types":"C","runs":11}

--- paint.java
+++ paint.java
@@ -3,7 +3,7 @@ import acm.program.GraphicsProgram;
 
 public class paint extends GraphicsProgram{
 	public void run(){
-GRect rect = new GRect(400, 400);
+GRect rect = new GRect(300, 300);
 	add(rect, 30, 30);
 	
 	
