e71a884ad355ec94b0fb1aa862f3e02ffab0de0d
S <s>
2023-10-12 17:39:22 +0400

    {"types":"C","runs":16}

--- cons.java
+++ cons.java
@@ -9,7 +9,7 @@ public void run(){
 	println((a + b + c)/3);
 	
 	double d = (1 / a) + (1 / b) + (1 / c) ;
-	System.out.print(1/a);
-	//println( 1 / d );
+	//System.out.print(1/a);
+	println( 1 / d );
 }
 }
