00ddd111c7de49378abca566ff85e1754567ca3a
S <s>
2023-10-12 17:19:26 +0400

    {"types":"C","runs":9}

--- cons.java
+++ cons.java
@@ -8,8 +8,8 @@ public void run(){
 	int c = readInt("enter third number : ");
 	println((a + b + c)/3);
 	
-	double d = (1/a) + (1/b) + (1/c) ;
+	double d = (1 / a) + (1 / b) + (1 / c) ;
 	
-	print(1/d);
+	println( 1 / d );
 }
 }
