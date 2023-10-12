c07eaeb1977ff8bdc848b7f77a22b36a2cd2ab62
S <s>
2023-10-12 17:15:01 +0400

    {"types":"C","runs":6}

--- cons.java
+++ cons.java
@@ -5,9 +5,11 @@ public void run(){
 	
 	int a = readInt("enter first number : ");
 	int b = readInt("enter second number : ");
-	println((a + b)/2);
+	int c = readInt("enter third number : ");
+	println((a + b + c)/3);
 	
+	int d = 1/a + 1/b + 1/c ;
 	
-	
+	println(1/d);
 }
 }
