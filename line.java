842d68899f981f05221acee90d68567c5b37d61f
S <s>
2023-10-13 10:27:34 +0400

    {"types":"C","runs":18}

--- line.java
+++ line.java
@@ -4,9 +4,18 @@ import acm.program.GraphicsProgram;
 public class line extends GraphicsProgram {
 public void run(){
 	
-	GLine lin = new GLine(0, getHeight()/2, getWidth(),  getHeight()/2);
-	add(lin);
-		
+//	GLine lin = new GLine(0, getHeight()/2, getWidth(),  getHeight()/2);
+//	add(lin);
 	
+	
+	
+	
+	for(int i = 1; i < 20; i++){
+		
+		GLine lin = new GLine( i* getWidth()/20 , 0, i* getWidth()/20,  getHeight());	
+		
+		add(lin);
+		
+	}
 }
 }
