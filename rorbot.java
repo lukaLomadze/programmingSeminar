31dd7fe0cea3b8f052d4a4f42e7c1d0a3007503c
S <s>
2023-10-13 10:50:05 +0400

    {"types":"C","runs":33}

--- rorbot.java
+++ rorbot.java
@@ -31,7 +31,7 @@ public class rorbot extends GraphicsProgram{
 		mounth.setColor(Color.black);
 		add(mounth, x+50, y+140);
 		
-		GRect nose = new GRect(10, 75);
+		GRect nose = new GRect(10, 50);
 		nose.setFilled(true);
 		nose.setColor(Color.black);
 		add(nose,x+95, y+45 );
