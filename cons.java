88cf16fe08b975a6b5916c421db071795c42935a
S <s>
2023-10-17 01:22:59 +0400

    {"types":"C","runs":10}

--- cons.java
+++ cons.java
@@ -6,10 +6,10 @@ public class cons extends ConsoleProgram {
 		int a = readInt("enter first number : ");
 		int b = readInt("enter second number : ");
 		int c = readInt("enter third number : ");
-		//println((a + b + c)/3);
+		
 		
 		double d = (double)((1 /(double) a) + (1 / (double) b) + (1 / (double)c)) ;
-		//System.out.print(1/a);
+		
 		println( 1 / d );
 		
 		
