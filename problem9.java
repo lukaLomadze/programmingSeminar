020fbd141d88dc7b8e6f912138825838eddde4ea
S <s>
2023-10-17 01:15:56 +0400

    {"types":"C","runs":7}

--- problem9.java
+++ problem9.java
@@ -1,4 +1,88 @@
+import stanford.karel.SuperKarel;
 
-public class problem9 {
-
+public class problem9 extends SuperKarel{
+	public void run(){
+		
+		
+		move();
+		move();
+		
+		
+		while(beepersPresent()){
+			pickBeeper();
+			turnAround();
+			move();
+			turnAround();
+			while(beepersPresent()){
+				pickBeeper();
+				move();
+				move();
+				putBeeper();
+				move();
+				putBeeper();
+				turnAround();
+				move();
+				move();
+				move();
+				turnAround();
+				
+			}
+		
+			move();
+			move();
+			move();
+			
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			turnAround();
+				
+				while(beepersPresent()){
+					pickBeeper();
+					turnAround();
+					move();
+					move();
+					move();
+					putBeeper();
+					turnAround();
+					move();
+					move();
+					move();
+					
+					
+					
+				}
+			turnAround();
+			move();
+			move();
+			turnAround();
+		
+		
+		
+		}
+		
+		
+		move();
+		move();
+		move();
+		while(beepersPresent()){
+			pickBeeper();
+			turnAround();
+			move();
+			move();
+			move();
+			turnAround();
+			putBeeper();
+			move();
+			move();
+			move();	
+		}
+		
+		
+	}
+	
+	
+	
+	
 }
