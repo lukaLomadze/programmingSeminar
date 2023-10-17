93e38b12afc84ee24bb06a6542c7bd7236c74c4d
S <s>
2023-10-17 17:51:30 +0400

    {"types":"C","runs":5}

--- problem18.java
+++ problem18.java
@@ -2,9 +2,13 @@ import acm.program.ConsoleProgram;
 
 public class problem18 extends ConsoleProgram{
 	public void run(){
-	
-		
-		
+	int a = readInt("enter number : ");
+	int b = readInt("enter number : ");
+		if (a >= b){
+			println(a);
+		}else{
+			println(b);
+		}
 		
 		
 	}
