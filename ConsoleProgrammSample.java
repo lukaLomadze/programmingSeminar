a1b49fd85d2905fd09233f2238933e79798f93ab
S <s>
2023-10-17 12:03:46 +0400

    {"types":"C","runs":11}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -10,38 +10,83 @@ import acm.program.*;
 public class ConsoleProgrammSample extends ConsoleProgram {
 	private static final int SENTINEL = 0; 
 	public void run() {
+		int b ;
 		
-//		int n = readInt("enter number : ");
-//		for(int i =0; i<n; i++){
-//			println("hello" + i);
-			int k =0;
-			
-			
+		while(true){
 			int a = readInt("enter number : ");
-			if(a != SENTINEL){
-				k = a;
+			
+			if(a == SENTINEL) {
+				break ;
 			}
-				
-				
-				
-			while(true){
-				int b = readInt("enter number : ");
-				
-				if(b == SENTINEL) {
-					break ;
-				}
-				
-				if (b > k){
-					k = b;
-					
-				}
-			}
-			println(k);
+			b=a;
+			
+			
+			
+			
+			
 			
 		}
 		
 		
 		
-	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int n = readInt("enter number : ");
+//		for(int i =0; i<n; i++){
+//			println("hello" + i);
+		
+		
+		
+		
+//			int k =0;
+//			
+//			
+//			int a = readInt("enter number : ");
+//			if(a != SENTINEL){
+//				k = a;
+//			}
+//				
+//				
+//				
+//			while(true){
+//				int b = readInt("enter number : ");
+//				
+//				if(b == SENTINEL) {
+//					break ;
+//				}
+//				
+//				if (b > k){
+//					k = b;
+//					
+//				}
+//			}
+//			println(k);
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+}
 
 
