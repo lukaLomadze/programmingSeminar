324065a1d5c96086601c7c0bcf6a752459c9189a
S <s>
2023-10-24 19:38:50 +0400

    {"types":"C","runs":5}

--- GraphicsProgramSample.java
+++ GraphicsProgramSample.java
@@ -12,11 +12,11 @@ public class GraphicsProgramSample extends GraphicsProgram {
 	
 	public void run() {
 	
-	
+		GRect squ = sqr(80);
 		
 		for(int i = 1; i<=5;i++){
 			
-			add(sqr(80), i* 80,i*80);
+			add(squ, i* 80,i*80);
 			
 		}
 		
