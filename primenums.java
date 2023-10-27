414773cc39d52f5c56e502acc165b2078da8f846
S <s>
2023-10-27 13:26:28 +0400

    {"types":"C","runs":12}

--- primenums.java
+++ primenums.java
@@ -6,11 +6,8 @@ public class primenums extends ConsoleProgram{
 		int d = 0;
 		for(int i = 2; i < 1000000; i++){
 			if(isprime(i)){
-				
 				d++;
-			}
-			
-			
+			}	
 		}
 		
 		println(d);
@@ -20,8 +17,8 @@ public class primenums extends ConsoleProgram{
 		
 	}
 	private boolean isprime (int n){
-		for(int i = 1; i*i  < n; i++){
-			if(n%i==0){
+		for(int i = 1; i * i  < n; i++){
+			if(n % i == 0){
 					return false;
 			}
 				
