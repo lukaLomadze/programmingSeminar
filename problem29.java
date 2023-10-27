357fcc989800e08bee00884f7846ea30a15f3ea2
S <s>
2023-10-27 10:36:19 +0400

    {"types":"C","runs":5}

--- problem29.java
+++ problem29.java
@@ -4,35 +4,30 @@ public class problem29 extends ConsoleProgram{
 	
 	
  public void run(){
-	 //int n = readInt("enter numb ");
-	 int f = 12456;
 	
-	 
-	// reverse(f);
-	 
- println(sum(f));
+	 int f = 12456;
+	 println(sum(f));
 	 
  }
  
- private int sum (int f){
- int sum = 0;
- 
- while(f>0){
-	 int na = f %10;
-	 f /= 10;
-	 sum += na;
+	 private int sum (int f){
+		 int sum = 0;
+		 
+		 while(f>0){
+			 int na = f %10;
+			 f /= 10;
+			 sum += na;
+			 
+			 
+		 }
+		
+		 return sum;
 	 
 	 
- }
-
- return sum;
- 
- 
- 
- 
- 
- }
- 
+	 
+	 
+	 
+	 }
  
  
  
@@ -43,19 +38,8 @@ public class problem29 extends ConsoleProgram{
  
  
  
- 
-// private int reverse(int f){
-//	 int res = 0;
-//	 while(f > 0){
-//		 int d = n % 10;
-//		 res = res*10+ d;
-//		 
-//	 }
-//	 
-// }
  
  
  
  
 }
-// 0, 1, 1, 2, 3, 5, 8, 
