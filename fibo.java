cfa8384a3ddfac9e09448a83dc08f2d08b67a27c
S <s>
2023-10-27 10:36:20 +0400

    {"types":"C","runs":5}

--- fibo.java
+++ fibo.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
 
-public class fibo {
-
+public class fibo extends ConsoleProgram{
+	public void run(){
+		int n = readInt("enter number ");
+		println(fib(n));
+		
+		
+	}
+	
+	
+	
+	private int fib(int n){
+		int a =1, b =1;
+		if(n == 1){
+			return a;
+		}
+		for (int i = 3; i<=n; i++){
+			int c = a+ b;
+			a=b;
+			b = c;
+			
+			
+			
+		}
+		
+		return b;
+	}
 }
