eee96aae6db54538789be012c106606266d98f62
S <s>
2023-10-27 10:54:38 +0400

    {"types":"C","runs":7}

--- primenums.java
+++ primenums.java
@@ -1,4 +1,36 @@
+import acm.program.ConsoleProgram;
 
-public class primenums {
-
+public class primenums extends ConsoleProgram{
+	public void run(){
+		
+		int c =0;
+		for(int i = 2; i < 1000000; i++){
+			if(isprime(i)){
+				
+				c++;
+			}
+			
+			
+		}
+		
+		println(c);
+		
+		
+		
+		
+	}
+	private boolean isprime(int n){
+for(int i = 1; i*i  < n; i++){
+		if(n%i==0){
+				return false;
+		}
+			
+			
+		}
+return true ;
+		
+		
+	}
+	
+	
 }
