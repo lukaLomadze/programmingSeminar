ed8ac6d737437c273b6b8c4c2c240350edb2e070
S <s>
2023-11-02 17:27:42 +0400

    {"types":"C","runs":40}

--- problem35.java
+++ problem35.java
@@ -3,7 +3,10 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class problem35 extends ConsoleProgram {
+	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	
 	public void run(){
 		
 		int t =0;
