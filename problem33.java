d7b089b045622c3955e6029c5fb52dadf97192c0
S <s>
2023-11-02 16:58:39 +0400

    {"types":"C","runs":19}

--- problem33.java
+++ problem33.java
@@ -1,9 +1,17 @@
+import java.awt.Color;
+
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class problem33 extends GraphicsProgram {
 	private RandomGenerator tr = RandomGenerator.getInstance();
 	public void run(){
+		
+		GRect rect = new GRect(tr.nextInt(100), tr.nextInt(100));
+		rect.setFilled(tr.nextBoolean());
+		rect.setColor(Color.tr.nextColor());
+		
 		
 		
 		
