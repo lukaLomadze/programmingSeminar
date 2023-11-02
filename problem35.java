fab90db4c9c9b22ea76cbbdea8920c6cdaea2287
S <s>
2023-11-02 17:15:24 +0400

    {"types":"C","runs":31}

--- problem35.java
+++ problem35.java
@@ -1,4 +1,28 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class problem35 {
-
+public class problem35 extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run(){
+		
+		int t =0;
+		int f =0;
+		
+		for(int i = 0; i< 10000; i++){
+		if(rgen.nextBoolean()){
+			t++;
+		}else{
+			f++;
+		}
+		println(t/ (double)(t+f));
+		
+		
+		
+	}	
+		
+	
+		
+		
+		
+}
 }
