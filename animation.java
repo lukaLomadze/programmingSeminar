df2c85ea6d3866d1248e4056a6560fb012b131e8
S <s>
2023-11-07 17:48:47 +0400

    {"types":"C","runs":2}

--- animation.java
+++ animation.java
@@ -1,8 +1,16 @@
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class animation extends GraphicsProgram {
 	public void run(){
 		
+		
+		GOval oval = new GOval(50 , 50);
+		add(oval);
+		
+		while(oval.getX()+50 <= getWidth() ){
+			oval.move(2, 2);
+		}
 		
 		
 	}
