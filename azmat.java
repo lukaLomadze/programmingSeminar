67c1cac12c0599ec7f7aba06bd42bb3a101244f5
S <s>
2023-11-09 16:38:04 +0400

    {"types":"C","runs":1}

--- azmat.java
+++ azmat.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class azmat {
-
+public class azmat extends ConsoleProgram{
+ public void run(){
+	 
+	 int n = 3;
+	 int m = 5;
+	 int k = n*m;
+	 
+	 int e = 3;
+	 
+	 int d =0;
+	 for(int i = 0; i< k; i++){
+		 
+		 if((i * e) % k == 1 ){
+			 d = i;
+		 }
+		 
+		 
+	 }
+	 println(d);
+	 
+ }
 }
