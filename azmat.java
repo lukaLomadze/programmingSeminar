6fee3d20bd1f1d4c920109ae05d2c3f069ef42c3
S <s>
2023-11-09 17:23:41 +0400

    {"types":"C","runs":5}

--- azmat.java
+++ azmat.java
@@ -1,25 +1,27 @@
 import acm.program.ConsoleProgram;
 
-public class azmat extends ConsoleProgram{
- public void run(){
-	 
-	 int n = 3;
-	 int m = 5;
-	 int k = n*m;
-	 
-	 int e = 3;
-	 
-	 int d = 0 ;
-	 
-	 for(int i = 0; i< ((n-1)*(m-1)); i++){
-		 
-		 if((i * e) % ((n-1)*(m-1)) == 1 ){
-			 d = i;
-		 }
-		 
-		 
-	 }
-	 println(d);
-	 
- }
+public class azmat extends ConsoleProgram {
+	public void run() {
+
+		int n = 3;
+		int m = 5;
+		int k = n * m;
+
+		int e = 3;
+		
+		//while()
+		int e = 3;
+
+		int d = 0;
+
+		for (int i = 0; i < ((n - 1) * (m - 1)); i++) {
+
+			if ((i * e) % ((n - 1) * (m - 1)) == 1) {
+				d = i;
+			}
+
+		}
+		println(d);
+
+	}
 }
