44d10aa1388fca5ce922d20394e965fcbf1c8a39
S <s>
2023-11-09 17:23:41 +0400

    {"types":"C","runs":5}

--- events.java
+++ events.java
@@ -1,4 +1,25 @@
+import java.awt.event.MouseEvent;
 
-public class events {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+public class events extends GraphicsProgram {
+	private GRect rect;
+	public void run() {
+
+		rect = new GRect(50, 50);
+		rect.setFilled(true);
+		add(rect, 50,50);
+		
+		
+		
+		
+		
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e){
+		rect.setLocation(e.getX(), e.getY());
+		
+	}
 }
