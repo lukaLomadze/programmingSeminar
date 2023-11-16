431c31bdf4114cb8074353abd9f8802d389af0c4
S <s>
2023-11-16 16:13:41 +0400

    {"types":"C","runs":19}

--- MIDTERM.java
+++ MIDTERM.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class MIDTERM {
-
+public class MIDTERM extends ConsoleProgram {
+public void run(){
+	
+	int n = 2;
+	int m = 8;
+	int log =0;
+	while(n<=m){
+		log++;
+		n *=n;
+		
+		
+	}
+	
+	println(log);
+	
+}
 }
