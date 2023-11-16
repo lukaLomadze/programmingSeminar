8bb679c9274b519523413c4ae558e2475f264e89
S <s>
2023-11-16 17:31:45 +0400

    {"types":"C","runs":36}

--- strings.java
+++ strings.java
@@ -8,12 +8,12 @@ public class strings extends ConsoleProgram {
 		//String d = readLine("enter str4");
 		
 		String  e = readLine("write") ;
-		String  f = readLine("write char");
+		char  f = readLine("write char");
 		
 		int n = 0;
 		
 		for(int i = 0; i < e.length(); i++){
-			if(f.equals(e.charAt(i))){
+			if(f == e.charAt(i)){
 				n++;
 			}
 		}
