63affc64437121a38155786757f38e6d6a715c50
S <s>
2023-11-16 17:26:35 +0400

    {"types":"C","runs":32}

--- strings.java
+++ strings.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
 
-public class strings {
-
+public class strings extends ConsoleProgram {
+	public void run() {
+		String a = "mari";
+		String b = "mari";
+		String c = new String("mari");
+		String d = readLine("enter str4");
+		
+		String  e = readLine("write") ;
+		String  f = readLine("write char");
+		
+		int n = 0;
+		
+		for(int i = 0; i < e.length(); i++){
+			if(f.equals(e.charAt(i))){
+				n++;
+			}
+		}
+		
+		
+		println("there are " + n + " " +f + " in " + e);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
