13b6685450a847b106e206bdb0fbe58835cb01c2
S <s>
2023-11-21 17:51:17 +0400

    {"types":"C","runs":3}

--- problem47.java
+++ problem47.java
@@ -1,10 +1,12 @@
 import acm.program.ConsoleProgram;
 
 public class problem47 extends ConsoleProgram{
-public void run(){
-	println(12500003 / 47000000.0);
+
+	public void run(){
 	
-	
+	if (true){
+		println("beng");
+	}
 	
 	
 }
