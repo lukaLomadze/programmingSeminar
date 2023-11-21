b6986544e1642b6860a3ec48e05a1933c9535b57
S <s>
2023-11-21 17:57:31 +0400

    {"types":"C","runs":3}

--- problem47.java
+++ problem47.java
@@ -1,13 +1,22 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class problem47 extends ConsoleProgram{
+public class problem47 extends GraphicsProgram {
 
-	public void run(){
-	
-	if (true){
-		println("beng");
+	private GRect rect = new GRect(10, 10, 10, 10);
+
+	public void run() {
+
+		add(rect, 10, 10);
+		addMouseListeners();
 	}
 	
-	
-}
+	public void mouseClicked(MouseEvent e){
+		if(getElementAt(e.getX(), e.getY()) == rect){
+			remove(rect);
+		}
+	}
 }
