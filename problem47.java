c25bb2079f8619ae954545f9a6816b00989bafe3
S <s>
2023-11-21 17:13:42 +0400

    {"types":"C","runs":1}

--- problem47.java
+++ problem47.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class problem47 {
-
+public class problem47 extends ConsoleProgram{
+public void run(){
+	println(12500003 / 47000000);
+	
+	
+	
+	
+}
 }
