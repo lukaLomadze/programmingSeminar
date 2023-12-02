6aeeead4e39c624d37a67e236c4e6189e1433ab9
S <s>
2023-12-02 15:51:23 +0400

    {"types":"C","runs":1}

--- Cut.java
+++ Cut.java
@@ -1,25 +1,35 @@
 import acm.program.ConsoleProgram;
 
 public class Cut extends ConsoleProgram {
-public void run(){
-	
-	String txt = "vakogioluka";
-	
-	for (int i = 0; i<txt.length(); i++){
+	public void run() {
+
+		String n = "luka lomadze0";
+		println('d'+1-'a'+"e"+3 + 2 +'b');
 		
-		String rep = "" + txt.charAt(i);
-		for (int k = 0; k<txt.length(); k++){
-		if(rep.equals(txt.charAt(k)+"")){
-			
-			
-		}
-			
-		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
-	
-	
+
 }
-	
-	
-	
-}
+
+// String txt = "vakogioluka";
+//
+// for (int i = 0; i<txt.length(); i++){
+//
+// String rep = "" + txt.charAt(i);
+// for (int k = 0; k<txt.length(); k++){
+// if(rep.equals(txt.charAt(k)+"")){
+//
+//
+// }
+//
+// }
+// }
+//
