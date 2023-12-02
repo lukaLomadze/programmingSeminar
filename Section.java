e9de9fca2e6c71ab23e84f59875d3ce7edc494da
S <s>
2023-12-02 11:39:15 +0400

    {"types":"C","runs":1}

--- Section.java
+++ Section.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Section {
+public class Section extends ConsoleProgram {
 
+	public void run(){
+		
+		String t = "iyo arabets rostevan mefe";
+		
+		for (int i = 0 ; i < t.length(); i++){
+			String a = t.substring(0, i);
+			String b = t.substring(i);	
+			println(a);
+			println(b);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
 }
