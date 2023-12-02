dfb0691aab94d72fe65345f0c41afa025b24ccc9
S <s>
2023-12-02 11:54:42 +0400

    {"types":"C","runs":5}

--- Section.java
+++ Section.java
@@ -3,19 +3,46 @@ import acm.program.ConsoleProgram;
 public class Section extends ConsoleProgram {
 
 	public void run(){
+		println(tr());
 		
-		String t = "iyo arabets rostevan mefe";
+				
+			
+			
+			}
+			
+			private boolean tr(){
+				String t = "iyo arabets rostevan mefe";
 		
 		for (int i = 1 ; i < t.length(); i++){
 			String a = t.substring(0, i);
 			String b = t.substring(i);	
 			println(a);
 			println(b);
+			for (int k = 1 ; k < a.length(); k++){
+			
+			
+				for (int l = 1 ; l < b.length(); l++){
+					String rep_a = "" + a.charAt(k);
+					a = a.replace(rep_a, "");
+					String rep_b = "" + a.charAt(l);
+					b = b.replace(rep_b, "");
+					String finall = a+ b; 
+					
+					if(finall.equals("yo arabts rostvan mf") ){
+						return true;
+					}
+				
+					
+				}
+				
+				
+			}
+			
 			
 			
 		}
 		
-		
+		return false;
 		
 		
 		
