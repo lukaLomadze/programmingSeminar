990112c26240c82e1da1e5e3677b58ce9b336856
S <s>
2023-12-02 12:06:46 +0400

    {"types":"C","runs":10}

--- Section.java
+++ Section.java
@@ -16,30 +16,30 @@ public class Section extends ConsoleProgram {
 		for (int i = 1 ; i < t.length(); i++){
 			String a = t.substring(0, i);
 			String b = t.substring(i);	
-//			println(a);
-//			println(b);
-			for (int k = 0 ; k < a.length(); k++){
-			
-			
-				for (int l = 0 ; l < b.length(); l++){
-					
-					String rep_a = "" + a.charAt(k);
-					String aa = a.replace(rep_a, "");
-					
-					String rep_b = "" + a.charAt(l);
-					String bb = b.replace(rep_b, "");
-					
-					String finall = aa + bb; 
-					
-					if(finall.equals("yo arabts rostvan mf") ){
-						return true;
-					}
-				
-					
-				}
-				
-				
-			}
+			println(a);
+			println(b);
+//			for (int k = 0 ; k < a.length(); k++){
+//			
+//			
+//				for (int l = 0 ; l < b.length(); l++){
+//					
+//					String rep_a = "" + a.charAt(k);
+//					String aa = a.replace(rep_a, "");
+//					
+//					String rep_b = "" + a.charAt(l);
+//					String bb = b.replace(rep_b, "");
+//					
+//					String finall = aa + bb; 
+//					
+//					if(finall.equals("yo arabts rostvan mf") ){
+//						return true;
+//					}
+//				
+//					
+//				}
+//				
+//				
+//			}
 			
 			
 			
