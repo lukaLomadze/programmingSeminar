924f96065ca992218dc59e7ca5aac9a3fa1a408c
S <s>
2023-12-05 17:44:36 +0400

    {"types":"C","runs":3}

--- assig.java
+++ assig.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class assig {
-
+public class assig extends ConsoleProgram {
+public void run(){
+	String r = "lukaluka";
+	println(r.indexOf("u"));
+	
+	
+	
+	
+}
 }
