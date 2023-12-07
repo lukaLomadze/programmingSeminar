6fd042d1081f97c842e6746734abb928eec4d4cf
S <s>
2023-12-07 17:51:52 +0400

    {"types":"C","runs":3}

--- problem53.java
+++ problem53.java
@@ -4,7 +4,7 @@ import acm.program.ConsoleProgram;
 
 public class problem53 extends ConsoleProgram {
 	public void run() {
-		//int n = readInt("enter number :");
+		// int n = readInt("enter number :");
 		sortNumbers();
 
 	}
@@ -12,33 +12,26 @@ public class problem53 extends ConsoleProgram {
 	private void sortNumbers() {
 		ArrayList<Integer> nums = new ArrayList<Integer>();
 
-//		for (int i = 0; i < n; i++) {
-//			int a = readInt("??");
-//			nums.add(a);
-//
-//		}
+		
 		nums.add(2);
 		nums.add(3);
 		nums.add(3);
 		nums.add(4);
 		nums.add(15);
-		
-		
-		for (int i = 0; i < 5; i++) {
-		int a = findMin(nums , i);
-nums.remove(a);
-nums.add(a);
-		
-		
-		
+
+		for (int i = 0; i < nums.size(); i++) {
+			int a = findMin(nums, i);
+			nums.remove(nums.indexOf(a));
+			nums.add(a);
+
 		}
 
 	}
 
 	private int findMin(ArrayList<Integer> nums, int i2) {
 		int min = nums.get(0);
-		for (int i = 1; i < nums.size() -i2 ; i++) {
-			if(min <  nums.get(i)){
+		for (int i = 1; i < nums.size() - i2; i++) {
+			if (min < nums.get(i)) {
 				min = nums.get(i);
 			}
 		}
