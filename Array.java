9d2e47bf9f77fb66d73460b2dae80559998e6685
S <s>
2023-12-08 12:56:50 +0400

    {"types":"C","runs":9}

--- Array.java
+++ Array.java
@@ -5,29 +5,26 @@ import acm.program.ConsoleProgram;
 public class Array extends ConsoleProgram {
 	public void run() {
 		ArrayList<Integer> nums = new ArrayList<Integer>();
-while(true){
-	int n = readInt("@");
-	if(n == -1){
-		break;
-	}
-	nums.add(0,n);
-}
-	//reset(nums);	
-		
-println(nums);
-		
-		
-		
-		
+		while (true) {
+			int n = readInt("@");
+			if (n == -1) {
+				break;
+			}
+			nums.add(0, n);
+		}
+		// reset(nums);
+
+		println(nums);
+
 	}
 
 	private void reset(ArrayList<Integer> nums) {
-		for(int i =1; i<= nums.size(); i++){
+		for (int i = 1; i <= nums.size(); i++) {
 			int a = nums.get(nums.size() - i);
 			nums.remove(nums.indexOf(a));
-			//nums.set(i-1, a);
+			// nums.set(i-1, a);
 			nums.add(0, a);
 		}
-		
+
 	}
 }
