3f4fe5303e7f5f86f89e2a4efea58a9dafc8fb6c
S <s>
2023-12-08 12:47:20 +0400

    {"types":"C","runs":1}

--- Array.java
+++ Array.java
@@ -1,14 +1,33 @@
+import java.util.ArrayList;
+
 import acm.program.ConsoleProgram;
 
 public class Array extends ConsoleProgram {
 	public void run() {
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+while(true){
+	int n = readInt("@");
+	if(n == -1){
+		break;
+	}
+	nums.add(n);
+}
+	reset(nums);	
+		
+		
+		
+		
+		
+		
+	}
 
-		
-		
-		
-		
-		
-		
+	private void reset(ArrayList<Integer> nums) {
+		for(int i =0; i< nums.size(); i++){
+			int a = nums.get(nums.size() - i);
+			//nums.remove(nums.indexOf(a));
+			nums.set(i, a);
+			
+		}
 		
 	}
 }
