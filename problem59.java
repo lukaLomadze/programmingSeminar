1622ef31d4e6bc87f6f1ca8aa2c8beea071a6ced
S <s>
2023-12-12 17:06:00 +0400

    {"types":"C","runs":1}

--- problem59.java
+++ problem59.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
 
-public class problem59 {
+import acm.program.ConsoleProgram;
 
+public class problem59 extends ConsoleProgram{
+	public void run(){
+		//n*m
+		int n = 5;
+		int m = 6;
+		//int[][]arr = {{}};
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		ArrayList<ArrayList<Integer>> matr = new ArrayList<ArrayList<Integer>>();
+		for(int i = 0 ; i<n; i++){
+			for(int i1 = 0 ; i1<n-i; i1++){
+				for(int i2 = 0 ; i2<m; i2++){
+					
+					for(int i3 = 0 ; i3<m-i2; i3++){
+						
+						
+					}	
+				}	
+				
+			}	
+	
+		}
+		
+		
+		
+	}
 }
