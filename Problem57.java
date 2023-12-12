f3121e9fc6467a8ee21a93a181e729a563cc70fa
S <s>
2023-12-12 17:17:28 +0400

    {"types":"C","runs":2}

--- Problem57.java
+++ Problem57.java
@@ -1,10 +1,30 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class Problem57 extends ConsoleProgram {
 	public void run() {
 
+		String a = "luka";
+		String b = "aulk";
 		
+	println(isanagram(a,b));
+	}
+
+	private boolean isanagram(String a, String b) {
+		check(a);
 		
+		return Arrays.equals(check(a), check(b));
+	}
+
+	private int[] check(String a) {
+		int[] arr = new int[26];
+		for(int i =0; i<a.length(); i++){
+			arr[i-'a']++;
+			
+			
+		}
+		return arr;
 		
 	}
 }
