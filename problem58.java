1c48ac23164caf23d4334899947a15cf42c7e3c8
S <s>
2023-12-12 17:37:56 +0400

    {"types":"C","runs":8}

--- problem58.java
+++ problem58.java
@@ -3,58 +3,44 @@ import java.util.ArrayList;
 import acm.program.ConsoleProgram;
 
 public class problem58 extends ConsoleProgram {
-	public void run(){
-		int [][] arr = new int [5][5];
+	public void run() {
+		int[][] arr = new int[5][5];
 		int a = 1;
-		for(int i = 0; i<arr.length ; i++){
-			for(int k = arr[0].length -1; k>= 0; k--){
-			arr[k][i]= a;
-					a++;
-				}
-		
+		for (int i = 0; i < arr.length; i++) {
+			for (int k = arr[0].length - 1; k >= 0; k--) {
+				arr[k][i] = a;
+				a++;
+			}
+
 		}
-		
-		
-		
+
 		println(isMagic(arr));
-		
-		
-		
-		
+
 	}
 
 	private boolean isMagic(int[][] arr) {
 
-		
-		if(arr.length != arr[0].length) {
-			return false;	
+		if (arr.length != arr[0].length) {
+			return false;
 		}
-		ArrayList<Integer> nums = new ArrayList<Integer>() ; 
-		for(int i = 1; i<=25 ; i++){
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		for (int i = 1; i <= 25; i++) {
 			nums.add(i);
-			
-			
+
 		}
-		for(int i = 0; i<arr.length ; i++){
-			for(int k = 0; k<arr[0].length ; k++){
-				if(nums.contains(arr[i][k])){
-					nums.remove((Integer)arr[i][k]);
-					
+		for (int i = 0; i < arr.length; i++) {
+			for (int k = 0; k < arr[0].length; k++) {
+				if (nums.contains(arr[i][k])) {
+					nums.remove((Integer) arr[i][k]);
 				}
-				
-				
-				
-				
 			}
-			
-			
 		}
-		
-		if(nums.isEmpty()){
-			return true;	
-			
+
+		if (nums.isEmpty()) {
+			return true;
+
 		}
-	return false;	
+		return false;
 	}
 
 }
