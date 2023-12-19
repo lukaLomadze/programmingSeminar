1224fadb8a2ee01eea1f20121135511c03b400a1
S <s>
2023-12-20 03:38:39 +0400

    {"types":"C","runs":3}

--- problem63.java
+++ problem63.java
@@ -1,4 +1,21 @@
+import java.util.HashMap;
+import java.util.Set;
 
-public class problem63 {
+import acm.program.ConsoleProgram;
 
+public class problem63 extends ConsoleProgram {
+	public void run() {
+		HashMap<String, Set<String>> names = new HashMap<String, Set<String>>();
+		while (true) {
+
+			String a = readLine("11");
+			String b = readLine("22");
+			if (a.equals(""))break;
+	
+			
+			
+			
+		}
+
+	}
 }
