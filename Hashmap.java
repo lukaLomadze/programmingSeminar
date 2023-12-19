b488e41a8e4fba9f770ec839b92293632e3bfa2a
S <s>
2023-12-19 11:39:39 +0400

    {"types":"C","runs":1}

--- Hashmap.java
+++ Hashmap.java
@@ -1,10 +1,29 @@
+import java.util.HashMap;
+
 import acm.program.ConsoleProgram;
 
 public class Hashmap extends ConsoleProgram {
 	public void run() {
-
+HashMap<String, Integer> names = new HashMap<String, Integer>();
+		while(true){
+			
+			String a = readLine("@");
+			
+			if(a.equals("")) break;
+			
+			if(names.containsKey(a)){
+				int b = names.get(a);
+				names.put(a, b + 1);
+				
+			}else{
+				names.put(a, 0);	
+		}
+		}
 		
-		
+		for(String name : names.keySet()){
+			println( name + "  :  " + names.get(name));
+			
+		}
 		
 		
 	}
