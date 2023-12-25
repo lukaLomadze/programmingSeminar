d5c3bf5055e72a97bfb014bc1a95f7a2000f89fd
S <s>
2023-12-25 15:51:18 +0400

    {"types":"C","runs":1}

--- Check.java
+++ Check.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
 
-public class Check {
+import acm.program.ConsoleProgram;
 
+public class Check extends ConsoleProgram{
+
+	public void run(){
+		
+		int[] nums = {1,1,3,3,3};
+		Arrays.sort(nums);
+		int n = 0;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] - nums[i - 1] == 0) {
+				n++;
+			}
+		}
+		
+		println(n);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
 }
