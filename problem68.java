8550a0d5c8678b94a4f37c3fd66990f9be622f44
S <s>
2023-12-26 17:15:41 +0400

    {"types":"C","runs":1}

--- problem68.java
+++ problem68.java
@@ -1,4 +1,30 @@
+import java.awt.event.ActionEvent;
 
-public class problem68 {
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
+import acm.program.GraphicsProgram;
+
+
+
+public class problem68 extends GraphicsProgram {
+	
+	JButton btn = new JButton("enter");
+		JTextField txt = new JTextField(10);
+	
+	public void run() {
+
+		
+		add(txt, SOUTH);
+		add(btn, SOUTH);
+		
+		
+		
+	}
+	public void actionPerformed(ActionEvent e){
+		
+		println(txt.getText());
+		
+		
+	}
 }
