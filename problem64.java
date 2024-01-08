dcd861ef53198d913f026a8694e875bb72cf4d11
S <s>
2024-01-08 17:36:34 +0400

    {"types":"C","runs":103}

--- problem64.java
+++ problem64.java
@@ -1,4 +1,63 @@
 
-public class problem64 {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.StringTokenizer;
 
+import acm.program.ConsoleProgram;
+
+public class problem64 extends ConsoleProgram {
+	public void run() {
+		HashMap<String, ArrayList<String>> names = new HashMap<String, ArrayList<String>>();
+		while (true) {
+
+			String a = readLine("11");
+			
+			if (a.equals(""))break;
+	
+			StringTokenizer token = new StringTokenizer(a);
+			String friend1 = token.nextToken();
+			String friend2 = token.nextToken();
+			names.putIfAbsent(friend1, new ArrayList<String>() );
+		if(!names.get(friend1).contains(friend2)){
+			names.get(friend1).add(friend2);
+		}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		for(String i : names.keySet()){
+		println( i + " : " + names.get(i).toString());
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String name = "";
+//		int num = 0;
+//		
+//		
+//		println(name);
+		
+		
+
+	}
 }
+
