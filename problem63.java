47d7c29b24ac4f2ecd08b2a8335043aa9095167f
S <s>
2024-01-08 17:31:21 +0400

    {"types":"C","runs":100}

--- problem63.java
+++ problem63.java
@@ -1,21 +1,42 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
 public class problem63 extends ConsoleProgram {
 	public void run() {
-		HashMap<String, Set<String>> names = new HashMap<String, Set<String>>();
+		HashMap<String, ArrayList<String>> names = new HashMap<String, ArrayList<String>>();
 		while (true) {
 
 			String a = readLine("11");
-			String b = readLine("22");
+			
 			if (a.equals(""))break;
 	
+			StringTokenizer token = new StringTokenizer(a);
+			String friend1 = token.nextToken();
+			String friend2 = token.nextToken();
+			names.putIfAbsent(friend1, new ArrayList<String>() );
+		if(!names.get(friend1).contains(friend2)){
+			names.get(friend1).add(friend2);
+		}
+			
+		}
+		String name = "";
+		int num = 0;
+		for(String i : names.keySet()){
+			if(num< names.get(i).size()){
+				name = i;
+			}
 			
 			
 			
 		}
+		
+		println(name);
+		
+		
 
 	}
 }
