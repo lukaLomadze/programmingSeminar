552c08ddb814827fb460471e385576d233820d80
S <s>
2024-01-20 00:41:31 +0400

    {"types":"C","runs":13}

--- Twodimmension.java
+++ Twodimmension.java
@@ -1,4 +1,18 @@
+import acm.program.ConsoleProgram;
 
-public class Twodimmension {
-
+public class Twodimmension  extends ConsoleProgram {
+	public void run() {
+		int[] b = { 1, 5 };
+		int[][] arr = new int [b.length][];
+		
+		for(int i =0; i< arr.length; i++){
+			arr[i]= new int[b[i]];
+			
+			
+			
+		}
+		println(arr.toString());
+		
+		
+	}
 }
