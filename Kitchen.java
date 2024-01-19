7784595e2f27abc0ba294388ab99fc8a1385f948
S <s>
2024-01-20 00:12:28 +0400

    {"types":"C","runs":0}

--- Kitchen.java
+++ Kitchen.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Kitchen {
+import acm.program.ConsoleProgram;
 
+public class Kitchen extends ConsoleProgram{
+public void run(){
+	HashMap <String, ArrayList<String>> dishes = new HashMap<>();
+	ArrayList<String> ingr = new ArrayList<>();
+	
+	ArrayList<String> kerz = new ArrayList<>();
+	
+	for(String i : dishes.keySet()){
+		
+		if(cannBeCooked(dishes.get(i) , ingr))kerz.add(i); 	
+	}
+
+}
+
+private boolean cannBeCooked(ArrayList<String> arrayList, ArrayList<String> ingr) {
+		for(String j : arrayList){
+				if(!ingr.contains(j)) return false;
+		}
+		return true;
+}
 }
