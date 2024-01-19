eeb917bbe73cc3c3195fa2299956f6a0d1e5a8c5
S <s>
2024-01-20 00:21:11 +0400

    {"types":"C","runs":1}

--- AddSpaces.java
+++ AddSpaces.java
@@ -1,4 +1,41 @@
 
-public class AddSpaces {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import acm.program.ConsoleProgram;
+
+public class AddSpaces extends ConsoleProgram{
+public void run(){
+	
+	String a = "ilovecoding";
+	String c = "";
+	int[] b = {1,5};
+	for(int i : b){
+
+		for(int k = 0; k < a.length(); k++){
+			if(i == k){
+				c += " ";	
+			}else{
+			c+= a.substring(k-1,k);
+			
+			}
+			
+		}
+		
+	}
+	
+	println(c);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
