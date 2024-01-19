de17fd1a794c023a8824a411895f260b289f1c97
S <s>
2024-01-20 00:32:44 +0400

    {"types":"C","runs":12}

--- AddSpaces.java
+++ AddSpaces.java
@@ -8,7 +8,7 @@ public class AddSpaces extends ConsoleProgram {
 	public void run() {
 
 		String a = "ilovecoding";
-		String c = "";
+	
 		int[] b = { 1, 5 };
 		for (int i =0; i< b.length; i++) {
 			String l = a.substring(0, b[i] + i) + " " + a.substring(b[i] + i );
