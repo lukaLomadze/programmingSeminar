a328ed82caa0828d9539d05c765807930f2c0396
S <s>
2024-01-20 13:23:21 +0400

    {"types":"C","runs":22}

--- CompressString.java
+++ CompressString.java
@@ -8,7 +8,7 @@ public void run(){
 	String s = "aabbbccdfff";
 	
 	for(int i =0; i< s.length();i++){
-		a.put(s.charAt(i), a.getOrDefault(a,0) + 1);
+		a.put(s.charAt(i), a.getOrDefault(s.charAt(i),0) + 1);
 		
 	}
 	String ret = "";
