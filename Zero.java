030d8bfc3e961f19025050c92dfa3a05026f99f9
S <s>
2024-01-20 14:00:18 +0400

    {"types":"C","runs":32}

--- Zero.java
+++ Zero.java
@@ -2,69 +2,53 @@ import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
-public class Zero extends ConsoleProgram{
-	public void run(){
-	int[][] nums = {{1, 0, 3, 4},
-	                {5, 6, 7, 8},
-	                {9, 0, 1, 2}};
-	String oo ="";
-	
-	int[][] nums2 = {{0, 0, 0, 0},
-                     {5, 0, 7, 8},
-                     {0, 0, 0, 0}};
-	for(int i = 0; i < nums.length; i++){
-		for(int k = 0; k < nums[0].length; k++){
-		
-			if(nums[i][k]==0){
-				oo+= ""+ i + "," +k+" ";
+public class Zero extends ConsoleProgram {
+	public void run() {
+		int[][] nums = { { 1, 0, 3, 4 }, { 5, 6, 7, 8 }, { 9, 0, 1, 2 } };
+		String oo = "";
+
+		int[][] nums2 = { { 0, 0, 0, 0 }, { 5, 0, 7, 8 }, { 0, 0, 0, 0 } };
+		for (int i = 0; i < nums.length; i++) {
+			for (int k = 0; k < nums[0].length; k++) {
+
+				if (nums[i][k] == 0) {
+					oo += "" + i + "," + k + " ";
+				}
+
 			}
-			
-			
-		}	
-		
-		
-	}
-	
-	
-	
-	StringTokenizer tok = new StringTokenizer(oo);
-	
-	while(tok.hasMoreTokens()){
-		StringTokenizer token = new StringTokenizer(tok.nextToken(), ",");
-		int a = Integer.parseInt(token.nextToken()) ;
-		int b = Integer.parseInt(token.nextToken()) ;
-		
-		zeroMatrix(a,b,nums);
-		
-		
-		
-		
-	}
-	
-	for(int i = 0; i < nums.length; i++){
-		for(int k = 0; k < nums[0].length; k++){
-		print(nums[i][k] + " ");
-		
-		}	
-		println();
-		
-	}
-	
-	
-	
-	
+
+		}
+
+		StringTokenizer tok = new StringTokenizer(oo);
+
+		while (tok.hasMoreTokens()) {
+			StringTokenizer token = new StringTokenizer(tok.nextToken(), ",");
+			int a = Integer.parseInt(token.nextToken());
+			int b = Integer.parseInt(token.nextToken());
+
+			zeroMatrix(a, b, nums);
+
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int k = 0; k < nums[0].length; k++) {
+				print(nums[i][k] + " ");
+
+			}
+			println();
+
+		}
+
 	}
 
 	private void zeroMatrix(int a, int b, int[][] nums) {
-	for(int i = 0; i< nums[0].length; i++){
-	nums[a][i]=0;
-	}
-		
-	for(int i = 0; i< nums.length; i++){
-		nums[i][b]=0;
+		for (int i = 0; i < nums[0].length; i++) {
+			nums[a][i] = 0;
 		}
-	
-	
-	
+
+		for (int i = 0; i < nums.length; i++) {
+			nums[i][b] = 0;
+		}
+
 	}
 }
