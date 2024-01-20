12108c25248dcc668fe64b1bd9b86aa1dcdf3cc2
S <s>
2024-01-20 13:22:31 +0400

    {"types":"C","runs":20}

--- CompressString.java
+++ CompressString.java
@@ -1,4 +1,23 @@
+import java.util.HashMap;
 
-public class CompressString {
+import acm.program.ConsoleProgram;
 
+public class CompressString extends ConsoleProgram{
+public void run(){
+	HashMap<Character, Integer> a = new HashMap<>();
+	String s = "aabbbccdfff";
+	
+	for(int i =0; i< s.length();i++){
+		a.put(s.charAt(i), a.getOrDefault(a,0) + 1);
+		
+	}
+	String ret = "";
+	for(char t : a.keySet()){
+		ret+= t + a.get(t);
+		
+		
+	}
+	println(ret);
+	
+}
 }
